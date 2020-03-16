5ffa3fe6db5ac2d03bbb6f86eb3fe18c187824d4
ravlasov <ravlasov@yandex.ru>
2020-03-17 00:24:49 +0300

    update from QGLWidget to QOpenGLWidget

--- lr4/OpenGLView.py
+++ lr4/OpenGLView.py
@@ -1,15 +1,16 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from PySide2.QtCore import QTimer
-from PySide2.QtOpenGL import QGLWidget
+from PySide2.QtWidgets import QOpenGLWidget
 
 
-class OpenGLView(QGLWidget):
+class OpenGLView(QOpenGLWidget):
     frameRate = 30
+    visionAngleHorizontal = 90.0
 
     def initializeGL(self):
         glMatrixMode(GL_PROJECTION)
-        gluPerspective(45.0, self.width() / self.height(), 0.1, 100.0)
+        gluPerspective(self.visionAngleHorizontal, self.width() / self.height(), 0.1, 100.0)
         glMatrixMode(GL_MODELVIEW)
         self.__timer = QTimer()
         self.__timer.timeout.connect(self.repaint)
@@ -17,13 +18,14 @@ class OpenGLView(QGLWidget):
 
     def resizeGL(self, width, height):
         glViewport(0, 0, width, height)
-        gluPerspective(45.0, (width / height), 0.1, 100.0)
+        gluPerspective(self.visionAngleHorizontal, (width / height), 0.1, 100.0)
 
     def paintGL(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity()
-        glTranslatef(0.0, 12.0, -2.0)
-        glBegin(GL_POINTS)
+        glTranslatef(0.0, 0.0, -1)
+        glBegin(GL_LINES)
         glVertex2f(0, 0)
+        glVertex2f(0, 1)
         glEnd()
         glFlush()
