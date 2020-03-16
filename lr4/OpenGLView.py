d2f062ba38ac06da116646132c0be4e62bfa2ca6
ravlasov <ravlasov@yandex.ru>
2020-03-17 01:09:39 +0300

    rotate axis with mouse

--- lr4/OpenGLView.py
+++ lr4/OpenGLView.py
@@ -1,3 +1,4 @@
+from math import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from PySide2.QtCore import QTimer
@@ -7,6 +8,10 @@ from PySide2.QtWidgets import QOpenGLWidget
 class OpenGLView(QOpenGLWidget):
     frameRate = 30
     visionAngleHorizontal = 90.0
+    mousePosX = 0
+    mousePosY = 0
+    angleHorizontal = 0
+    angleVertical = 0
 
     def initializeGL(self):
         glMatrixMode(GL_PROJECTION)
@@ -24,8 +29,26 @@ class OpenGLView(QOpenGLWidget):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glLoadIdentity()
         glTranslatef(0.0, 0.0, -1)
-        glBegin(GL_LINES)
-        glVertex2f(0, 0)
-        glVertex2f(0, 1)
-        glEnd()
+        gluLookAt(0.0, 0.0, 1.0,
+                  0.0, 0.0, 0.0,
+                  0.0, 1.0, 0.0)
+        glRotatef(self.angleVertical, 1.0, 0.0, 0.0)
+        glRotatef(self.angleHorizontal, 0.0, 1.0, 0.0)
         glFlush()
+
+    def rotateLeftAroundOY(self, point, angle):
+        return [
+            point[0] * cos(angle * pi / 180) + point[2] * sin(angle * pi / 180),
+            point[1],
+            - point[0] * sin(angle * pi / 180) + point[2] * cos(angle * pi / 180)
+        ]
+
+    def mousePressEvent(self, event):
+        self.mousePosX = event.pos().x()
+        self.mousePosY = event.pos().y()
+
+    def mouseMoveEvent(self, event):
+        self.angleHorizontal += (event.x() - self.mousePosX)
+        self.angleVertical += (event.y() - self.mousePosY)
+        self.mousePosX = event.x()
+        self.mousePosY = event.y()
