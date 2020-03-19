1b181ae4804d5397d8fa8e120e3d2230bc91abe6
ravlasov <ravlasov@yandex.ru>
2020-03-19 23:25:53 +0300

    interpolation code cleared

--- lr4/SplineInterpolate.py
+++ lr4/SplineInterpolate.py
@@ -1,4 +1,3 @@
-# Структура, описывающая сплайн на каждом сегменте сетки
 class SplineTuple:
     def __init__(self, a, b, c, d, x):
         self.a = a
@@ -7,25 +6,14 @@ class SplineTuple:
         self.d = d
         self.x = x
 
-
-# Построение сплайна
-# x - узлы сетки, должны быть упорядочены по возрастанию, кратные узлы запрещены
-# y - значения функции в узлах сетки
-# n - количество узлов сетки
 def BuildSpline(x, y, n):
-    # Инициализация массива сплайнов
     splines = [SplineTuple(0, 0, 0, 0, 0) for _ in range(0, n)]
     for i in range(0, n):
         splines[i].x = x[i]
         splines[i].a = y[i]
-
     splines[0].c = splines[n - 1].c = 0.0
-
-    # Решение СЛАУ относительно коэффициентов сплайнов c[i] методом прогонки для трехдиагональных матриц
-    # Вычисление прогоночных коэффициентов - прямой ход метода прогонки
     alpha = [0.0 for _ in range(0, n - 1)]
     beta = [0.0 for _ in range(0, n - 1)]
-
     for i in range(1, n - 1):
         hi = x[i] - x[i - 1]
         hi1 = x[i + 1] - x[i]
@@ -36,12 +24,8 @@ def BuildSpline(x, y, n):
         z = (A * alpha[i - 1] + C)
         alpha[i] = -B / z
         beta[i] = (F - A * beta[i - 1]) / z
-
-    # Нахождение решения - обратный ход метода прогонки
     for i in range(n - 2, 0, -1):
         splines[i].c = alpha[i] * splines[i + 1].c + beta[i]
-
-    # По известным коэффициентам c[i] находим значения b[i] и d[i]
     for i in range(n - 1, 0, -1):
         hi = x[i] - x[i - 1]
         splines[i].d = (splines[i].c - splines[i - 1].c) / hi
@@ -49,19 +33,16 @@ def BuildSpline(x, y, n):
     return splines
 
 
-# Вычисление значения интерполированной функции в произвольной точке
 def Interpolate(splines, x):
     if not splines:
-        return None  # Если сплайны ещё не построены - возвращаем NaN
-
+        return None
     n = len(splines)
     s = SplineTuple(0, 0, 0, 0, 0)
-
-    if x <= splines[0].x:  # Если x меньше точки сетки x[0] - пользуемся первым эл-тов массива
+    if x <= splines[0].x:
         s = splines[0]
-    elif x >= splines[n - 1].x:  # Если x больше точки сетки x[n - 1] - пользуемся последним эл-том массива
+    elif x >= splines[n - 1].x:
         s = splines[n - 1]
-    else:  # Иначе x лежит между граничными точками сетки - производим бинарный поиск нужного эл-та массива
+    else:
         i = 0
         j = n - 1
         while i + 1 < j:
@@ -71,7 +52,5 @@ def Interpolate(splines, x):
             else:
                 i = k
         s = splines[j]
-
     dx = x - s.x
-    # Вычисляем значение сплайна в заданной точке по схеме Горнера (в принципе, "умный" компилятор применил бы схему Горнера сам, но ведь не все так умны, как кажутся)
-    return s.a + (s.b + (s.c / 2.0 + s.d * dx / 6.0) * dx) * dx;
+    return s.a + (s.b + (s.c / 2.0 + s.d * dx / 6.0) * dx) * dx
