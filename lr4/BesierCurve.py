ae35eb00d0e06f8fec13b83307896df3f2048025
ravlasov <ravlasov@yandex.ru>
2020-03-19 22:23:55 +0300

    bug fixed

--- lr4/BesierCurve.py
+++ lr4/BesierCurve.py
@@ -6,8 +6,8 @@ def binomial(n, k):
         return 0
     return factorial(n)/(factorial(n-k)*factorial(k))
 
-def calculateBesierCurve(self, P):
-    degree = len(P)
+def calculateBesierCurve(P):
+    degree = len(P) - 1
     T = [np.array([t ** (degree - i) for i in range(0, degree + 1)]) for t in np.arange(0, 1.001, 0.001)]
     besie_matrix = np.array([[((-1) ** ((n + k) % 2)) * binomial(degree, n) * binomial(n, k) for k in range(0, degree + 1)] for n in range(degree, -1, -1)])
     return [np.dot(np.dot(T[i], besie_matrix), P) for i in range(0, len(T))]
