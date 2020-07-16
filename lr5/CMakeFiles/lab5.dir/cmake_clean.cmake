1150f1ea1f52218357bd792cf03dcdc01ac72fc7
ravlasov <ravlasov@yandex.ru>
2020-07-16 14:51:35 +0300

    lr5

---
+++ lr5/CMakeFiles/lab5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab5.dir/main.cpp.o"
+  "CMakeFiles/lab5.dir/Shader.cpp.o"
+  "CMakeFiles/lab5.dir/Camera.cpp.o"
+  "lab5.pdb"
+  "lab5"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
